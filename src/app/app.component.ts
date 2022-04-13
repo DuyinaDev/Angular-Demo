c5c6bb0172ebef77320f69c839c7c148a3ed895d
DuyinaDev <maria.a.guzman@accenture.com>
2022-04-13 17:04:35 -0500

    Basic TodoApp in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //import
 
-@Component({
+@Component({ // Metadata and template 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { //define propiedades y metodos que necesitamos Vista
   title = 'my-app-ang';
+  list:any[] = [];
+  addTask(item:string ){
+    this.list.push({id:this.list.length, name:item})
+    
+  }
+  removeTask(id:number){
+    console.log(id);
+    this.list = this.list.filter(item=> item.id!== id);
+    
+  }
 }
